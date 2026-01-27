03c9ae000e798b75c622223c0e69b2a46efcb437
Igor-75 <idecamposoliveira9021@gmail.com>
2026-01-26 21:24:13 -0300

    primeiro commit

---
+++ bootcamp.js
@@ -0,0 +1,5 @@
+console.log("Lista Vôlei SIBS - 19h")
+console.log("local:https://maps.app.goo.gl/KBJyxBfeB5pWWJsB9")
+for (let contador = 1; contador < 19; contador++){
+  console.log(contador + ". ")
+}
